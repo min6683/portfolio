0f14a7987c1d370ee83830bc322551d244a9fffe
min6683 <dev.sm6683@gmail.com>
2022-04-21 01:39:14 +0900

    두번째  수정

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 window.onload = function () {
-    
+    let sw_cc = new Swiper('.sw-cc', {
+        slidesPerView: 3,
+        loop: false,
+        pagination: {
+            el: '.sw-cc-pg',
+            clickable: true,
+        },
+        spaceBetween: 30,
+    });
 };
 
 $(document).ready(function(){
