f9a4a6c7b30b5f985a47010a281954c53abe9c6a
min6683 <dev.sm6683@gmail.com>
2022-04-21 22:47:40 +0900

    세번째 수정

--- js/script.js
+++ js/script.js
@@ -8,6 +8,16 @@ window.onload = function () {
         },
         spaceBetween: 30,
     });
+
+    // 위로 가기
+    let gotop = $('.gotop');
+    gotop.click(function (event) {
+        event.preventDefault();
+        $('html').animate({
+            scrollTop: 0
+        }, 500);
+    });
+
 };
 
 $(document).ready(function(){
